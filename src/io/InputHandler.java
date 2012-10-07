234a126b15d48f9b06a872f96b9a71a89deb9a99
Jiao Jing Ping <elleryjiao@gmail.com>
2012-10-07 16:45:44 +0800

    modify input handler

--- src/io/InputHandler.java
+++ src/io/InputHandler.java
@@ -1,71 +1,34 @@
 package io;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 import msg.Message;
 
 import pipe.Pipe;
 
-public class InputHandler implements Runnable{
-	private String filePath;
+public class InputHandler implements Runnable {
+	private InputStreamReader inStream;
 	private Pipe outPipe;
 
-	public InputHandler(Pipe pipe) {
-		// TODO Auto-generated constructor stub
-//		this constructor is to return an InputHanler which takes input from terminal
+	public InputHandler(Pipe pipe, InputStreamReader inStream) {
 		this.outPipe = pipe;
-		this.filePath = null;
-	}
-	
-	public InputHandler(Pipe pipe, String filePath) {
-//		This constructs an InputHandler read a file specified by filePath as input
-		this.outPipe = pipe;
-		this.filePath = filePath;
+		this.inStream = inStream;
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		if (filePath != null){
-//			read input from file
-				readingFile();
-		}
-		else{
-			readingCommandLine();
-		}
+		read();
 	}
 
-	private void readingCommandLine() {
+	private void read() {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(inStream);
 		String line;
-		while(!(line=sc.nextLine()).isEmpty()){
+		while (!(line = sc.nextLine()).isEmpty()) {
 			outPipe.write(new Message(line));
 		}
 		sc.close();
 	}
-
-	private void readingFile() {
-		FileReader fr;
-		try {
-			fr = new FileReader(this.filePath);
-			BufferedReader br = new BufferedReader(fr);
-			String currentLine;
-			while ((currentLine = br.readLine()) != null){
-				outPipe.write(new Message(currentLine));
-			}
-			fr.close();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
 }
