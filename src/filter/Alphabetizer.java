e520ca3b28bac75e3b654bdb8fcac7569c319de0
Jiao Jing Ping <elleryjiao@gmail.com>
2012-10-07 18:07:51 +0800

    modify controller

--- src/filter/Alphabetizer.java
+++ src/filter/Alphabetizer.java
@@ -6,32 +6,35 @@ import java.util.PriorityQueue;
 import msg.Message;
 import pipe.Pipe;
 import util.StringComparator;
-public class Alphabetizer extends InputFilter{
-    Comparator<String> comparator = new StringComparator();
 
-	PriorityQueue<String > sortedTitles = new PriorityQueue<String>(0,comparator);
-	
+public class Alphabetizer extends InputFilter {
+	Comparator<String> comparator = new StringComparator();
+
+	PriorityQueue<String> sortedTitles = new PriorityQueue<String>(0,
+			comparator);
+
 	public Alphabetizer(Pipe inPipe, Pipe outPipe) {
-		super(inPipe, outPipe);		
+		super(inPipe, outPipe);
 	}
+
 	public void run() {
 		while (true) {
 			assert (sortedTitles != null);
 			assert (sortedTitles.isEmpty());
-			
-			while(true){
+
+			while (true) {
 				Message val = inPipe.read();
-				//TODO fix this!
-								
-				if (val.getClass() != message.EofMessage.class ){
-					sortedTitles.add(val.getContent());				
-				}else{
+				// TODO fix this!
+
+				if (val.getClass() != msg.EofMessage.class) {
+					sortedTitles.add(val.getContent());
+				} else {
 					break;
-				}				
-			}			
-			while(!sortedTitles.isEmpty()){
+				}
+			}
+			while (!sortedTitles.isEmpty()) {
 				outPipe.write(new Message(sortedTitles.poll()));
-			}			
+			}
 		}
 	}
 }
