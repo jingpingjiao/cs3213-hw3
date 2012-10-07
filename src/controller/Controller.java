d670651499bf1e721387f851d1f3db6e77440fbe
Jiao Jing Ping <elleryjiao@gmail.com>
2012-10-07 16:41:02 +0800

    add ouphandler outputfilter and controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,26 +1,91 @@
 package controller;
 
-
-import io.InputHandler;
-import pipe.MyPipe;
-import pipe.Pipe;
-
-import filter.Filter;
-import filter.InputFilter;
-import filter.OutputFilter;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Controller {
+	private BufferedReader br;
+
+	public Controller() {
+		this.setBr(new BufferedReader(new InputStreamReader(System.in)));
+	}
+
 	public static void main(String args[]) {
-		Pipe pipe1 = new MyPipe();
-		Pipe pipe2 = new MyPipe();
-		Pipe pipe3 = new MyPipe();
+		Controller c = new Controller();
+		c.run();
+	}
 
+	private void run() {
+		// default input
+		String input = "exit";
+		String[] args;
+
+		// welcome routine
+		welcome();
+		help();
+
+		// read input and process
+		do {
+			try {
+				System.out.print(">");
+				input = br.readLine().trim();
+				args = input.split(" ");
+				if (args.length < 1) {
+					continue;
+				}
+
+				String command = args[0];
+				if (command.equalsIgnoreCase("exit")) {
+					this.exit();
+				} else if (command.equalsIgnoreCase("help")) {
+					this.help();
+				} else if (command.equalsIgnoreCase("process")) {
+					this.process(args);
+				} else {
+					this.help();
+				}
+			} catch (IOException e) {
+				System.out.println("Error occurs while reading input");
+			}
+
+		} while (true);
+
+	}
+
+	private static void welcome() {
+		String s = "=======================================\n"
+				+ "          WELCOME TO KWIC MINI\n"
+				+ "=======================================\n";
+		System.out.println(s);
+
+	}
+
+	private void exit() {
+		System.out.println("Exiting from KIWIC MINI...");
+		System.exit(0);
+	}
+
+	private void help() {
+		String helpMsg = "help : display help content";
+		String exitMsg = "exit : exit from KIWIC MINI";
+		String processMsg = "process : process input, default input from stdin,output to stdout  \n"
+				+ "-i	[filename] specify input file, \n"
+				+ "-o	[filename] specify output file.";
+		String help = "Avaible commands are: \n" + helpMsg + "\n" + exitMsg
+				+ "\n" + processMsg + "\n";
+		System.out.println(help);
+	}
+
+	private void process(String[] args) {
 		
-		InputHandler inputHandler = new InputHandler(pipe1);
+	}
 
-		Filter inputFilter = new InputFilter(pipe1, pipe2);
-		Filter outputFilter = new OutputFilter(pipe2, pipe3);
-		inputFilter.run();
-		outputFilter.run();
+	public BufferedReader getBr() {
+		return br;
+	}
+
+	public void setBr(BufferedReader br) {
+		this.br = br;
 	}
 }
--- src/filter/OutputFilter.java
+++ src/filter/OutputFilter.java
@@ -1,8 +1,10 @@
 package filter;
 
+import msg.Message;
+import pipe.MyPipe;
 import pipe.Pipe;
 
-public class OutputFilter extends Filter{
+public class OutputFilter extends Filter {
 
 	public OutputFilter(Pipe inPipe, Pipe outPipe) {
 		super(inPipe, outPipe);
@@ -11,8 +13,18 @@ public class OutputFilter extends Filter{
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (!((MyPipe) inPipe).empty()) {
+			Message msg = inPipe.read();
+			Message outMsg = process(msg);
+			sendMessage(outMsg);
+		}
 	}
 
+	private void sendMessage(Message outMsg) {
+		outPipe.write(outMsg);
+	}
+
+	private Message process(Message msg) {
+		return msg;
+	}
 }
